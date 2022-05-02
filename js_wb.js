21ce8b8e6283f0a82158d54f82ee39ae42acf678
polshkov <6399700@gmail.com>
2022-05-02 14:56:15 +0300

    конспект

---
+++ js_wb.js
@@ -0,0 +1,26 @@
+let yolo4ka4 = 4;
+let yolo4ka6 = 6;
+let yolo4ka_name = "trust";
+
+let result = yolo4ka4 + yolo4ka6;
+let result2 = yolo4ka4 + yolo4ka6 + " " + yolo4ka_name;
+let result3 = yolo4ka_name + ` ${yolo4ka4 + yolo4ka6}`;
+
+console.log(result);
+console.log(yolo4ka4 + yolo4ka6);
+
+console.log(result2);
+console.log(yolo4ka4 + yolo4ka6 + " trust")
+
+console.log(result3);
+console.log(`trust ${yolo4ka4 + yolo4ka6}`)
+
+console.log(yolo4ka_name);
+
+
+// if (result == 11){
+//     let ggg = 5+5;
+//     console.log("trust " + ggg)
+// } else {
+//     let fff = 5+6;
+//     console.log("trust " + fff) }
